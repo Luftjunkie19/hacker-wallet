2513e0bad0d01072d705153eda1e97544e1f52d3
Luftjunkie19 <luftjunkie@protonmail.com>
2025-10-06 21:05:43 +0200

    refactor: update permissions in manifest, enhance background script with database handling, and improve content script message handling

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,38 @@
+
+import {openDB} from 'idb';
+
 export {}
 
 console.log('Call now here');
+
+chrome.runtime.onMessage.addListener(async(
+   message, sender,sendResponse
+)=>{
+
+const dbName="web3-wallet-data";
+const VER=1; const 
+sessionDb= async ()=>{
+    return openDB(dbName, VER, {
+        upgrade(database) {
+            if(!database.objectStoreNames.contains("web3-wallet-data")) database.createObjectStore('web3-wallet-data');
+        },
+    })
+};
+
+const db= await sessionDb();
+const element = await db.get("web3-wallet-data", "currentConnectedNetwork");
+
+console.log(element);
+
+    console.log('Message received');
+    console.log(message);
+    console.log(sender);
+
+    sendResponse({
+        network:element.networkName,
+        url:element.rpcURL,
+        from:'Background'
+    });
+
+
+})
--- src/content.ts
+++ src/content.ts
@@ -1,3 +1,31 @@
 export {}
 
-console.log('Call to the context !');
+(function (){
+    window.hackerWallet={
+        name:"Hacker Wallet",
+        id:'hackerWallet',
+        on:()=>{},
+    }
+
+
+})();
+
+
+
+window.addEventListener('message', (event)=>{
+ 
+if(event.data.target === 'hacker-wallet'){
+
+    chrome.runtime.sendMessage(
+        event.data
+    );
+
+}
+
+if(event.data.from && event.data.from === 'Background'){
+    console.log(event.data);
+}
+
+
+});
+
